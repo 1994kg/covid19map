52d49e2788b599522e338f0d9bc7a35ef4ed83b1
Ju Hyun Park <57860235+1994kg@users.noreply.github.com>
2020-10-30 01:48:27 +0900

    Add files via upload

---
+++ js/demo/chart-pie-demo.js
@@ -0,0 +1,35 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["완치자(23,030명)", "격리자(1,421명)", "사망자(438명)"],
+    datasets: [{
+      data: [92.5308, 5.70935, 1.75981],
+      backgroundColor: ['#1cc88a', '#f6c23e', '#FF0000'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#e74a3b'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
